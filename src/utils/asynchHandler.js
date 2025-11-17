149c798f7e550d0c12c7d94feb1ac98c386699b4
Vinayak Mathur <vinayakmathur8@gmail.com>
2025-11-17 09:32:04 +0530

    added util files

--- src/utils/asynchHandler.js
+++ src/utils/asynchHandler.js
@@ -1,6 +1,6 @@
-const asyncHandler = (reqestHandler) => {
+const asyncHandler = (requestHandler) => {
       return (req, res, next) => {
-        Promise.resolve(reqestHandler(req, res, next))
+        Promise.resolve(requestHandler(req, res, next))
         .catch((err) => next(err));
     };
 };
