0c94b3dc9994fbdbd0ae88c7935ac1b29c031b38
Vinayak Mathur <vinayakmathur8@gmail.com>
2025-11-17 08:57:47 +0530

    added util files

---
+++ src/utils/APIResponse.js
@@ -0,0 +1,9 @@
+class APIResponse{
+    constructor(statuscode, data, message = "Success"){
+        this.statuscode = statuscode,
+        this.data = data,
+        this.message  = message,
+        this.success = statuscode < 400
+    }
+
+}{}
