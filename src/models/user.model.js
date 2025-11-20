5b1776e6d71099191bfd15e2ff11428d2ea03d6c
Vinayak Mathur <vinayakmathur8@gmail.com>
2025-11-20 22:57:16 +0530

    Added JWT auth and decrpyt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,9 +55,38 @@ UserSchema.pre("save", async function (next) {
     next()
 })
 
-UserSchema.method.isPasswordCorrect = async function (password) {
+UserSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password,this.password)
     
 }
+
+UserSchema.methods.genrateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+UserSchema.methods.genrateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
 
 export const User = mongoose.model("User",UserSchema)
